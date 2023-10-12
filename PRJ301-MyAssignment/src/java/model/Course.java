26998658ab8b6d9000945fcb3dff432c1fb8149e
phomuadong123 <145014481+phomuadong123@users.noreply.github.com>
2023-10-12 20:56:05 +0700

    update

---
+++ PRJ301-MyAssignment/src/java/model/Course.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+
+/**
+ *
+ * @author duong
+ */
+public class Course {
+    private int courseId;
+    private String name;
+    private String code;
+
+    public Course() {
+    }
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
