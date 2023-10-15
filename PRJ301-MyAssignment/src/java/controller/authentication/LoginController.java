465ea6f7287f00fc5e14934ad496f1de353d9705
phomuadong123 <145014481+phomuadong123@users.noreply.github.com>
2023-10-15 20:39:42 +0700

    update file

---
+++ PRJ301-MyAssignment/src/java/controller/authentication/LoginController.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller.authentication;
+
+import dal.AccountDBContext;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Account;
+
+/**
+ *
+ * @author sonnt
+ */
+public class LoginController extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("view/authentication/login.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        Account param = new Account();
+        
+        param.setUsername(username);
+        param.setPassword(password);
+
+        AccountDBContext db = new AccountDBContext();
+        Account loggedUser = db.login(username, password);
+
+        if (loggedUser == null) {
+            response.getWriter().println("incorrect username or password");
+        } else {
+            String remember = request.getParameter("remember");
+            
+            HttpSession session = request.getSession();
+            session.setAttribute("account", loggedUser);
+            
+            if (remember != null) {
+                Cookie c_user = new Cookie("user", username);
+                Cookie c_pass = new Cookie("pass", password);
+                c_user.setMaxAge(3600 * 24);
+                c_pass.setMaxAge(3600 * 24);
+                response.addCookie(c_user);
+                response.addCookie(c_pass);
+            }
+
+            response.getWriter().println("Hello " + loggedUser.getDisplayname());
+        }
+        
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
